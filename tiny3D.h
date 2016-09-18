4bc21c35401070c70898d0578e6d5c187bf2ae53
lixuefeng <sdlwlxf@163.com>
2016-09-18 22:38:14 +0800

    first commit

---
+++ tiny3D.h
@@ -0,0 +1,214 @@
+// this tiny game engine is rendered by CPU. I make it for learing the pipline and rasterization. This is the first step to realise my own game engine. so just do it!
+// modules:
+// 1. math library
+//  1). vector
+typedef struct {
+    float x, y, z, w;
+} vector_t;
+typedef vector_t point_t;
+
+float interp(float a, float b, float t) {
+    return b + (a - b) * t;
+}
+//      1)). length
+float vector_length(vector_t *v) {
+    return sqrt(v->x * v->x + v->y * v->y + v->z * v->z);
+}
+//      2)). add
+void vector_add(vector_t *c, const vector_t *a, const vector_t *b) {
+    c->x = a->x + b->x; 
+    c->y = a->y + b->y; 
+    c->z = a->z + b->z; 
+    c->w = 1.0f;
+}
+//      3)). sub
+void vector_sub(vector_t *c, const vector_t *a, const vector_t *b) {
+    c->x = a->x - b->x; 
+    c->y = a->y - b->y; 
+    c->z = a->z - b->z; 
+    c->w = 1.0f;
+}
+//      4)). dotproduct
+float vector_dotproduct(const vector_t *a, const vector_t *b) {
+    return a->x * b->x + a->y * b->y + a->z * b->z;
+}
+//      5)). crossproduct
+void vector_crossproduct(vector_t *c, const vector_t *a, const vector_t *b) {
+    c->x = a->y*b->z - a->z*b->y;
+    c->y = a->z*b->x - a->x*b->z;
+    c->z = a->x*b->y - a->y*b->x;
+    c->w = 1.0f;
+}
+//      6)). interp t[0, 1]
+void vector_interp(vector_t *c, const vector_t *a, const vector_t *b, float t) {
+    c->x = interp(a->x, b->x, t);
+    c->y = interp(a->y, b->y, t);
+    c->z = interp(a->z, b->z, t);
+    c->w = 1.0f;
+}
+//      7)). normalize
+void vector_normalize(vector_t *v) {
+    float k = 1 / vector_length(v);
+    v->x *= k;
+    v->y *= k;
+    v->z *= k;
+}
+//  2). matrix
+typedef struct {
+    float m[4][4];
+} matrix_t;
+//      1)). add
+void matrix_add(matrix_t *c, const matrix_t *a, const matrix_t *b) {
+    for(int i = 0; i < 4; i++)
+        for(int j = 0; j < 4; j++)
+            c->m[i][j] = a->m[i][j] + b->m[i][j];
+}
+//      2)). sub
+void matrix_sub(matrix_t *c, const matrix_t *a, const matrix_t *b) {
+    for(int i = 0; i < 4; i++)
+        for(int j = 0; j < 4; j++)
+            c->m[i][j] = a->m[i][j] - b->m[i][j];
+}
+//      3)). mul
+void matrix_mul(matrix_t *c, const matrix_t *a, const matrix_t *b) {
+    for(int i = 0; i < 4; i++)
+        for(int j = 0; j < 4; j++)
+            c->m[i][j] = a->m[i][0]*b->m[0][j] + a->m[i][1]*b->m[1][j] + a->m[i][2]*b->m[2][j] + a->m[i][3]*b->m[3][j];
+}
+//      4)). scale
+void matrix_scale(matrix_t *m, float k) {
+    for(int i = 0; i < 4; i++)
+        for(int j = 0; j < 4; j++)
+            m->m[i][j] *= k;
+}
+//      5)). apply v = v * m
+void matrix_apply(vector_t *v1, const vector_t *v2, const matrix_t *m) {
+    for(int j = 0; j < 4; j++)
+        v1[j] = v2[0] * m->m[0][j] + v2[1] * m->m[1][j] + v2[2] * m->[2][j] + v2[3] * m->m[3][j];
+}
+//      6)). set_identity
+void matrix_set_identity(matrix_t *m) {
+    for(int i = 0; i < 4; i++)
+        for(int j = 0; j < 4; j++)
+            if(i == j)
+                m->m[i][j] = 1.0f;
+            else
+                m->m[i][j] = 0.0f;
+}
+//      7)). set_zero
+void matrix_set_zero(matrix_t *m) {
+    for(int i = 0; i < 4; i++)
+        for(int j = 0; j < 4; j++)
+            m->m[i][j] = 0.0f;
+}
+//      8)). set_translate
+void matrix_set_translate(matrix_t *m, float dx, float dy, float dz) {
+    matrix_set_identity(m);
+    m->m[3][0] = dx;
+    m->m[3][1] = dy;
+    m->m[3][2] = dz;
+}
+//      9)). set_scale
+void matrix_set_scale(matrix_t *m, float sx, float sy, float sz) {
+    matrix_set_identity(m);
+    m->m[0][0] = dx;
+    m->m[1][1] = dy;
+    m->m[2][2] = dz;
+}
+//      10)).set_rotate m, x, y, z, theta
+// x*x*(1-cos0)+cos0    x*y*(1-cos0)+z*sin0   x*z*(1-cos0)-y*sin0
+// x*y*(1-cos0)-z*sin0  y*y*(1-cos0)+cos0     y*z*(1-cos0)+x*sin0
+// x*z*(1-cos0)+y*sin0  y*z*(1-cos0)-x*sin0   z*z*(1-cos0)+cos0
+void matrix_set_rotate(matrix_t *m, const vector_t *v, float theta) {
+    assert(fabs(v*v - 1.0f) < .01f);
+
+    float s = sin(theta);
+    float c = cos(theta);
+
+    float a = 1.0f - c;
+    float ax = a * v->x;
+    float ay = a * v->y;
+    float az = a * v->z;
+
+    m->m[0][0] = ax * v->x + c;
+    m->m[0][1] = ax * v->y + v->z * s;
+    m->m[0][2] = ax * v->z - v->y * s;
+
+    m->m[1][0] = ay * v->x - v->z * s;
+    m->m[1][1] = ay * v->y + c;
+    m->m[1][2] = ay * v->z + v->x * s;
+
+    m->m[2][0] = az * v->x + v->y * s;
+    m->m[2][1] = az * v->y - v->x * s;
+    m->m[2][2] = az * v->z + c;
+
+    m->m[3][0] = m->m[3][1] = m->m[3][2] = 0.0f;
+    m->m[0][3] = m->m[1][3] = m->m[2][3] = 0.0f;
+    m->m[3][3] = 1.0f;
+}
+//      11)).set_lookat m, eye, at, up 
+// zaxis = normal(At - Eye)
+// xaxis = normal(cross(Up, zaxis))
+// yaxis = cross(zaxis, xaxis)
+
+// xaxis.x           yaxis.x           zaxis.x          0
+// xaxis.y           yaxis.y           zaxis.y          0
+// xaxis.z           yaxis.z           zaxis.z          0
+//-dot(xaxis, eye)  -dot(yaxis, eye)  -dot(zaxis, eye)  1
+void matrix_set_lookat(matrix_t *m, const vector_t *eye, const vector_t *at, const vector_t *up) {
+    vector_t zaxis, xaxis, yaxis;
+    vector_sub(&zaxis, at, eye);
+    vector_normalize(&zaxis);
+    vector_crossproduct(&xaxis, up, &zaxis);
+    vector_normalize(&xaxis);
+    vector_crossproduct(&yaxis, &zaxis, &xaxis);
+
+    m->m[0][0] = xaxis.x;
+    m->m[0][1] = yaxis.x;
+    m->m[0][2] = zaxis.x;
+    m->m[1][0] = xaxis.y;
+    m->m[1][1] = yaxis.y;
+    m->m[1][2] = zaxis.y;
+    m->m[2][0] = xaxis.z;
+    m->m[2][1] = yaxis.z;
+    m->m[2][2] = zaxis.z;
+    m->m[3][0] = -vector_dotproduct(&xaxis, eye);
+    m->m[3][1] = -vector_dotproduct(&yaxis, eye);
+    m->m[3][2] = -vector_dotproduct(&zaxis, eye);
+    m->m[0][3] = m->m[1][3] = m->m[2][3] = 0.0f;
+    m->m[3][3] = 1.0f;
+}
+//      12)).set_perspective m, fovy, aspect, zn, zf
+// zoom = 1 / tan(fov/2)
+// zoomy = 1 / tan(fovy/2)
+// zoomx = zoomy * aspect
+// zoomx    0       0               0
+// 0        zoomy   0               0
+// 0        0       zf/(zf-zn)      1
+// 0        0       zn*zf/(zn-zf)   0
+void matrix_set_perspective(matrix_t *m, float fovy, float aspect, float zn, float zf) {
+    float zoomy = 1.0f / (float)tan(fovy * 0.5f);
+    float zoomx = zoomy / aspect;
+    m->m[0][0] = zoomx;
+    m->m[1][1] = zoomy;
+    m->m[2][2] = zf / (zf - zn);
+    m->m[3][2] = -m->m[2][2] * zn;
+    m->m[2][3] = 1;
+    m->m[0][1] = m->m[0][2] = m->m[0][3] = m->m[1][0] = m->m[1][2] = m->m[1][3] = 0.0f;
+    m->m[2][0] = m->m[2][1] = m->m[3][0] = m->m[3][1] = m->m[3][3] = 0.0f;
+}
+
+// I have realized the math library. Then I should realize the core data structure of computer graphics.
+// 2. transform_t
+typedef struct {
+    matrix_t world;
+    matrix_t view;
+    matrix_t projection;
+    matrix_t transform;
+    float w, h;
+} transform_t;
+//  1). transform_update (world * view * projection
+//  2). transform_init (ts, width, height)
+//  3). transform_apply
+//  4). transform_check_cvv(v)
+//  5). transform_homogenize(ts, y, x)
