69fa3f1314099371b366cbe069b6cd9d050f3866
lixuefeng <sdlwlxf@163.com>
2016-09-20 10:09:07 +0800

    hello

--- tiny3D.h
+++ tiny3D.h
@@ -209,7 +209,21 @@ typedef struct {
 } transform_t;
 
 //  1). transform_update (world * view * projection
+void transform_update(transform_t *ts) {
+    matrix_t m;
+    matrix_mul(&m, &ts->world, &ts->view);
+    matrix_mul(&ts->transform, &m, &ts->projection);
+}
 //  2). transform_init (ts, width, height)
+void transform_init(transform_t *ts, int width, int height) {
+    float aspect = (float)width / (float)height;
+    matrix_identity(&ts->world);
+    matrix_identity(&ts->view);
+    matrix_set_perspective(&ts->projection, 3.1415926 * 0.5f, aspect, 1.0f, 500.0f);
+    ts->w = (float)width;
+    ts->h = (float)height;
+    transform_update(ts);
+}
 //  3). transform_apply
 //  4). transform_check_cvv(v)
 //  5). transform_homogenize(ts, y, x)
