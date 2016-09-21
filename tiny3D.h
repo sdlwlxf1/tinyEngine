bcf098109e386a97338df09f4ced12a32965c293
lixuefeng <sdlwlxf@163.com>
2016-09-21 22:38:06 +0800

    third

--- tiny3D.h
+++ tiny3D.h
@@ -225,5 +225,84 @@ void transform_init(transform_t *ts, int width, int height) {
     transform_update(ts);
 }
 //  3). transform_apply
+void transform_apply(transform_t *ts, vector_t *y, vector_t *x) {
+    matrix_apply(y, x, &ts->transform);
+}
 //  4). transform_check_cvv(v)
+int transform_check_cvv(const vector_t *v) {
+    float w = v->w;
+    int check = 0;
+    if(v->z < 0.0f) check |= 1;
+    if(v->z > w) check |= 2;
+    if(v->x < -w) check |= 4;
+    if(v->x > w) check |= 8;
+    if(v->y < -w) check |= 16;
+    if(v->y > w) check |= 32;
+    return check;
+}
 //  5). transform_homogenize(ts, y, x)
+void transform_homogenize(const transform_t *ts, vector *y, const vector *x) {
+    float rhw = 1.0f / x->w;
+    y->x = (x->x * rhw + 1.0f) * 0.5f * ts->w;
+    y->y = (1.0f - x->y * rhw) * 0.5f * ts->h;
+    y->z = x->z * rhw;
+    y->w = 1.0f;
+}
+
+// 3. geometry calculation (vertex, scanline, border check, rect and so on)
+typedef struct { float r, g, b; } color_t;
+typedef struct { float u, v; } texcoord_t;
+typedef struct { point_t pos; texcoord_t tc; color_t color; float rhw; } vertex_t;
+
+typedef struct { vertex_t v, v1, v2; } edge_t;
+typedef struct { float top, bottom; edge_t left, right; } trapezoid_t;
+typedef struct { vertex_t v, step; int x, y, w; } scanline_t;
+
+void vertex_rhw_init(vertex_t *v) {
+    float rhw = 1.0f / v->pos->w;
+    v->rhw = rhw;
+    v->tc.u *= rhw;
+    v->tc.v *= rhw;
+    v->color.r *= rhw;
+    v->color.g *= rhw;
+    v->color.b *= rhw;
+}
+
+void vertex_interp(vertex_t *y, const vertex_t *x1, const vertex_t *x2, float k) {
+    vector_interp(&y->pos, &x1->pos, &x2->pos, k);
+    y->tc.u = interp(x1->tc.u, x2->tc.u, k);
+    y->tc.v = interp(x1->tc.v, x2->tc.v, k);
+    y->color.r = interp(x1->color.r, x2->color.r, k);
+    y->color.g = interp(x1->color.g, x2->color.g, k);
+    y->color.b = interp(x1->color.b, x2->color.b, k);
+    y->rhw = interp(x1->rhw, x2->rhw, k);
+}
+
+void vertex_division(vertex_t *y, const vertex_t *x1, const vertex_t *x2, float w) {
+    float inv = 1.0f / w;
+    y->pos.x = (x2->pos.x - x1->pos.x) * inv;
+    y->pos.y = (x2->pos.y - x1->pos.y) * inv;
+    y->pos.z = (x2->pos.z - x1->pos.z) * inv;
+    y->pos.w = (x2->pos.w - x1->pos.w) * inv;
+    y->tc.u = (x2->tc.u - x1->tc.u) * inv;
+    y->tc.v = (x2->tc.v - x1->tc.v) * inv;
+    y->color.r = (x2->color.r - x1->color.r) * inv;
+    y->color.g = (x2->color.g - x1->color.g) * inv;
+    y->color.b = (x2->color.b - x1->color.b) * inv;
+    y->rhw = (x2->rhw - x1->rhw) * inv;
+}
+
+void vertex_add(vertex_t *y, const vertex_t *x) {
+    y->pos.x += x->pos.x;
+    y->pos.y += x->pos.y;
+    y->pos.z += x->pos.z;
+    y->pos.w += x->pos.w;
+    y->tc.u += x->tc.u;
+    y->tc.v += x->tc.v;
+    y->color.r += x->color.r;
+    y->color.g += x->color.g;
+    y->color.b += x->color.b;
+    y->rhw += x->rhw;
+}
+
+
