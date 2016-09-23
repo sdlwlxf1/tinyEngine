8f7aec31dc0663ade2fec48dae458cabb260daa6
lixuefeng <sdlwlxf@163.com>
2016-09-23 18:54:32 +0800

    oh

--- tiny3D.h
+++ tiny3D.h
@@ -305,4 +305,102 @@ void vertex_add(vertex_t *y, const vertex_t *x) {
     y->rhw += x->rhw;
 }
 
+typedef struct {
+	transform_t transform;      // 坐标变换器
+	int width;                  // 窗口宽度
+	int height;                 // 窗口高度
+	IUINT32 **framebuffer;      // 像素缓存：framebuffer[y] 代表第 y行
+	float **zbuffer;            // 深度缓存：zbuffer[y] 为第 y行指针
+	IUINT32 **texture;          // 纹理：同样是每行索引
+	int tex_width;              // 纹理宽度
+	int tex_height;             // 纹理高度
+	float max_u;                // 纹理最大宽度：tex_width - 1
+	float max_v;                // 纹理最大高度：tex_height - 1
+	int render_state;           // 渲染状态
+	IUINT32 background;         // 背景颜色
+	IUINT32 foreground;         // 线框颜色
+}	device_t;
 
+#define RENDER_STATE_WIREFRAME      1		// 渲染线框
+#define RENDER_STATE_TEXTURE        2		// 渲染纹理
+#define RENDER_STATE_COLOR          4		// 渲染颜色
+
+// 设备初始化，fb为外部帧缓存，非 NULL 将引用外部帧缓存（每行 4字节对齐）
+void device_init(device_t *device, int width, int height, void *fb) {
+	int need = sizeof(void*) * (height * 2 + 1024) + width * height * 8;
+	char *ptr = (char*)malloc(need + 64);
+	char *framebuf, *zbuf;
+	int j;
+	assert(ptr);
+	device->framebuffer = (IUINT32**)ptr;
+    ptr += sizeof(void*) * height;
+    
+	device->zbuffer = (float**)ptr;
+	ptr += sizeof(void*) * height;
+
+	device->texture = (IUINT32**)ptr;
+	ptr += sizeof(void*) * 1024;
+
+	framebuf = (char*)ptr;
+	zbuf = (char*)ptr + width * height * 4;
+	ptr += width * height * 8;
+	if (fb != NULL) framebuf = (char*)fb;
+	for (j = 0; j < height; j++) {
+		device->framebuffer[j] = (IUINT32*)(framebuf + width * 4 * j);
+		device->zbuffer[j] = (float*)(zbuf + width * 4 * j);
+	}
+	device->texture[0] = (IUINT32*)ptr;
+	device->texture[1] = (IUINT32*)(ptr + 16);
+	memset(device->texture[0], 0, 64);
+	device->tex_width = 2;
+	device->tex_height = 2;
+	device->max_u = 1.0f;
+	device->max_v = 1.0f;
+	device->width = width;
+	device->height = height;
+	device->background = 0xc0c0c0;
+	device->foreground = 0;
+	transform_init(&device->transform, width, height);
+	device->render_state = RENDER_STATE_WIREFRAME;
+}
+
+void device_destroy(device_t *device) {
+	if (device->framebuffer) 
+		free(device->framebuffer);
+	device->framebuffer = NULL;
+	device->zbuffer = NULL;
+	device->texture = NULL;
+}
+
+void device_set_texture(device_t *device, void *bits, long pitch, int w, int h) {
+	char *ptr = (char*)bits;
+	int j;
+	assert(w <= 1024 && h <= 1024);
+	for (j = 0; j < h; ptr += pitch, j++) 	// 重新计算每行纹理的指针
+		device->texture[j] = (IUINT32*)ptr;
+	device->tex_width = w;
+	device->tex_height = h;
+	device->max_u = (float)(w - 1);
+	device->max_v = (float)(h - 1);
+}
+
+void device_clear(device_t *device, int mode) {
+	int y, x, height = device->height;
+	for (y = 0; y < device->height; y++) {
+		IUINT32 *dst = device->framebuffer[y];
+		IUINT32 cc = (height - 1 - y) * 230 / (height - 1);
+		cc = (cc << 16) | (cc << 8) | cc;
+		if (mode == 0) cc = device->background;
+		for (x = device->width; x > 0; dst++, x--) dst[0] = cc;
+	}
+	for (y = 0; y < device->height; y++) {
+		float *dst = device->zbuffer[y];
+		for (x = device->width; x > 0; dst++, x--) dst[0] = 0.0f;
+	}
+}
+
+void device_pixel(device_t *device, int x, int y, IUINT32 color) {
+	if (((IUINT32)x) < (IUINT32)device->width && ((IUINT32)y) < (IUINT32)device->height) {
+		device->framebuffer[y][x] = color;
+	}
+}
